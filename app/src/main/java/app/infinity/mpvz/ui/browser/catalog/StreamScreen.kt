42e9bdfa5c24bef6471735a7ba13f3c0fc2163cb
ZHINFINITY <2.03890437e+08+ZHINFINITY@users.noreply.github.com>
2026-09-09 21:19:46 +0000

    Fix stream loading state import

--- app/src/main/java/app/infinity/mpvz/ui/browser/catalog/StreamScreen.kt
+++ app/src/main/java/app/infinity/mpvz/ui/browser/catalog/StreamScreen.kt
@@ -2,6 +2,7 @@ package app.infinity.mpvz.ui.browser.catalog
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
