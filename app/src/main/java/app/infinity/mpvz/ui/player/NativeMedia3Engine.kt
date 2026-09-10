7f518f1c14aae640a1f86aaac17a8b3b06594bf8
ZHINFINITY <2.03890437e+08+ZHINFINITY@users.noreply.github.com>
2026-09-10 19:59:11 +0000

    Use Media3 extractor Chapter metadata type

--- app/src/main/java/app/infinity/mpvz/ui/player/NativeMedia3Engine.kt
+++ app/src/main/java/app/infinity/mpvz/ui/player/NativeMedia3Engine.kt
@@ -34,7 +34,7 @@ import androidx.media3.exoplayer.source.DefaultMediaSourceFactory
 import androidx.media3.exoplayer.source.ProgressiveMediaSource
 import androidx.media3.extractor.DefaultExtractorsFactory
 import androidx.media3.extractor.ExtractorsFactory
-import androidx.media3.extractor.metadata.matroska.Chapter
+import androidx.media3.extractor.metadata.Chapter
 import androidx.media3.extractor.text.DefaultSubtitleParserFactory
 import androidx.media3.extractor.text.SubtitleParser
 import androidx.media3.ui.CaptionStyleCompat
