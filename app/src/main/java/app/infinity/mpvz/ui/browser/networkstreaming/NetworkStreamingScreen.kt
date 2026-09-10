9ad490ae97ff61b80cf23a177f2e6723bb3e7dc0
ZHINFINITY <2.03890437e+08+ZHINFINITY@users.noreply.github.com>
2026-09-10 12:43:34 +0000

    Fix chooser TextButton import

--- app/src/main/java/app/infinity/mpvz/ui/browser/networkstreaming/NetworkStreamingScreen.kt
+++ app/src/main/java/app/infinity/mpvz/ui/browser/networkstreaming/NetworkStreamingScreen.kt
@@ -70,6 +70,7 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Tab
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
