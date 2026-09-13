62a872107b723a46ae8401b7d596f8b1808c2868
ZHINFINITY <zhjjk0@gmail.com>
2026-09-13 22:26:18 +0000

    build: depend on libass subtitle module

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -286,6 +286,7 @@ dependencies {
   implementation(libs.androidx.media3.effect)
   implementation(libs.androidx.media3.transformer)
   implementation(libs.jellyfin.media3.ffmpeg.decoder)
+  implementation(project(":subtitle-libass"))
   implementation(platform(libs.sora.editor.bom))
   implementation(libs.sora.editor)
   implementation(libs.sora.language.textmate)
