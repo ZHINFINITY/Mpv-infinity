b67caa60ce3a9fc2e8015817b8f33dda07a9f29c
ZHINFINITY <2.03890437e+08+ZHINFINITY@users.noreply.github.com>
2026-09-14 09:40:53 +0000

    fix: import Media3 text output from exoplayer

--- app/src/main/java/app/infinity/mpvz/ui/player/LibassRenderersFactory.kt
+++ app/src/main/java/app/infinity/mpvz/ui/player/LibassRenderersFactory.kt
@@ -2,9 +2,9 @@ package app.infinity.mpvz.ui.player
 
 import android.content.Context
 import android.os.Looper
-import androidx.media3.common.text.TextOutput
 import androidx.media3.exoplayer.DefaultRenderersFactory
 import androidx.media3.exoplayer.Renderer
+import androidx.media3.exoplayer.text.TextOutput
 import androidx.media3.subtitle.libass.DirectAssSubtitleRenderer
 import androidx.media3.subtitle.libass.LibassSubtitleRenderer
 import java.util.ArrayList
