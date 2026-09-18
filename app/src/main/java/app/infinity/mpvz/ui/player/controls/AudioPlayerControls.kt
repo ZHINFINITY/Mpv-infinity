8e3a2455ba99594475744845e9bf05f0abe87545
ZHINFINITY <zhjjk0@gmail.com>
2026-09-19 00:57:28 +0530

    Add safe-area imports for edge-to-edge visualizer

--- app/src/main/java/app/infinity/mpvz/ui/player/controls/AudioPlayerControls.kt
+++ app/src/main/java/app/infinity/mpvz/ui/player/controls/AudioPlayerControls.kt
@@ -63,6 +63,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.WindowInsetsSides
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
@@ -70,6 +71,7 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.safeDrawing
+import androidx.compose.foundation.layout.only
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.widthIn
