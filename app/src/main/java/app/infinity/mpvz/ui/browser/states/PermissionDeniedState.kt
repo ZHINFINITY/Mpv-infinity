677420bd4069eb2ac02248e75f713a324037c0e9
ZHINFINITY <2.03890437e+08+ZHINFINITY@users.noreply.github.com>
2026-09-20 10:48:16 +0000

    Fix badge layout compiler import

--- app/src/main/java/app/infinity/mpvz/ui/browser/states/PermissionDeniedState.kt
+++ app/src/main/java/app/infinity/mpvz/ui/browser/states/PermissionDeniedState.kt
@@ -78,6 +78,7 @@ import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextDecoration
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
